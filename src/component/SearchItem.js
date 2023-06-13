100556e800b8101d932fa73e1b905431565f8dc1
Semih Erdogan <82077230+semiherd@users.noreply.github.com>
2023-06-13 17:37:25 +0300

    Update SearchItem.js

--- src/component/SearchItem.js
+++ src/component/SearchItem.js
@@ -2,28 +2,28 @@ import { forwardRef } from "react";
 
 const SearchItem= forwardRef(({searchVal,setSearchVal,searchFn},ref) => {
 
-	const handleInputChange = (e) => {
+  const handleInputChange = (e) => {
     setSearchVal(e.target.value);
   }
   const handleSearch = () => {
-		ref.current.page= 1
-		searchFn({page:1,value:searchVal})
+	ref.current.page= 1
+	searchFn({page:1,value:searchVal})
   }
   const handleEnterPress = e => {
     if(e.key === 'Enter' && searchVal.length>1) handleSearch()
   }
 
-	return (
-		<>
-			<input
-				type="search"
-				placeholder="Search..."
-				value={searchVal}
-				onChange={handleInputChange}
-				onKeyDown={handleEnterPress}
-			/>
-			<button onClick={() => handleSearch()} disabled={searchVal.length<2?true:false}>Search</button>
-		</>
-	)
+  return (
+	<>
+		<input
+			type="search"
+			placeholder="Search..."
+			value={searchVal}
+			onChange={handleInputChange}
+			onKeyDown={handleEnterPress}
+		/>
+		<button onClick={() => handleSearch()} disabled={searchVal.length<2?true:false}>Search</button>
+	</>
+  )
 })
 export default SearchItem;
